b1a5b7790024d3be2864c9fed7b56b0626cb5472
osw <smartosw@korea.ac.kr>
2023-11-15 22:22:36 +0900

    first commit

---
+++ index.py
@@ -0,0 +1,42 @@
+import re
+import os
+import sys
+import google_stream_stt as gs  # stt 모듈
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import urllib.request
+from tqdm import tqdm
+
+import tensorflow
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from konlpy.tag import Okt
+from konlpy.tag import Kkma
+from keras import layers, models, optimizers, losses, metrics
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import textCussDetect as td
+
+
+# print(os.path.dirname(__file__))
+
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+gs.main()
+# stt 호출
+
+# 최초 1회 생성
+(
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    tokenizer,
+    train_data,
+    test_data,
+) = td.load_and_preprocess_data()
+
+# td.training() #필요시 실행
+td.sentiment_predict("와", tokenizer)
+td.sentiment_predict("와?", tokenizer)
---
+++ opencv_predict.py
@@ -0,0 +1,168 @@
+from PIL import Image
+import numpy as np
+import cv2
+import os
+
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)  # set video width
+cam.set(4, 480)  # set video height
+
+# before
+# face_detector = cv2.CascadeClassifier(
+#     'haarcascades/haarcascade_frontalface_default.xml')
+
+# After
+face_detector = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+# For each person, enter one numeric face id
+face_id = int(input("\n enter user id end press <return> ==>  "))
+face_name = input("\n enter user name end press <return> ==>  ")
+print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+
+# 딕셔너리 선언
+face_dict = {
+    -1: "None",
+}
+face_dict[face_id] = face_name
+# Initialize individual sampling face count
+count = 0
+while True:
+    ret, img = cam.read()
+    # img = cv2.flip(img, -1) # flip video image vertically
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    for x, y, w, h in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        count += 1
+        # Save the captured image into the datasets folder
+        cv2.imwrite("#dataset/User." + str(face_id) + "." + str(count) + ".jpg", img)
+        cv2.imshow("image", img)
+    k = cv2.waitKey(100) & 0xFF  # Press 'ESC' for exiting video
+    if k == 27:
+        break
+    elif count >= 30:  # Take 30 face sample and stop video
+        break
+    print(count)
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
+
+
+# ===============================학습
+
+
+# Path for face image database
+path = "#dataset"
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_detector = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    faceSamples = []
+    ids = []
+    for imagePath in imagePaths:
+        PIL_img = Image.open(imagePath).convert("L")  # convert it to grayscale
+        img_numpy = np.array(PIL_img, "uint8")
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        # print(id)
+        faces = face_detector.detectMultiScale(img_numpy)
+        for x, y, w, h in faces:
+            faceSamples.append(img_numpy[y : y + h, x : x + w])
+            ids.append(id)
+    return faceSamples, ids
+
+
+print("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+faces, ids = getImagesAndLabels(path)
+recognizer.train(faces, np.array(ids))
+
+# Save the model into trainer/trainer.yml
+# recognizer.save() worked on Mac, but not on Pi
+recognizer.write("#trainer/trainer." + str(face_id) + "." + str(face_name) + ".yml")
+# Print the numer of faces trained and end program
+print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+
+# ===============================예측
+print(face_dict)
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+# before
+# recognizer.read("#trainer/trainer.yml")
+# 디렉토리 경로
+directory = "#trainer/"  # 실제 디렉토리 경로로 대체해야 합니다
+
+# 모든 yml 파일 불러오기
+for filename in os.listdir(directory):
+    if filename.endswith(".yml"):
+        file_path = os.path.join(directory, filename)
+        recognizer.read(file_path)
+
+
+face_detector = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# iniciate id counter
+id = 0
+
+# names related to ids: example ==> loze: id=1,  etc
+# 이런식으로 사용자의 이름을 사용자 수만큼 추가해준다.
+# names = ["None", "osw", "ljy", "chs", "ksw"]
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)  # set video widht
+cam.set(4, 480)  # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
+
+while True:
+    ret, img = cam.read()
+    # img = cv2.flip(img, -1)  # Flip vertically
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_detector.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
+
+    for x, y, w, h in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        id, confidence = recognizer.predict(gray[y : y + h, x : x + w])
+        # Check if confidence is less them 100 ==> "0" is perfect match
+
+        if confidence < 100 and id in face_dict:
+            id = face_dict[id]
+            confidence = "  {0}%".format(round(100 - confidence))
+
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+            roi = img[y : y + h, x : x + w]
+            # cv2.GaussianBlur(roi, (가로 불투명도, 세로 불투명도), 0)
+            blurred_roi = cv2.GaussianBlur(roi, (75, 75), 0)
+            img[y : y + h, x : x + w] = blurred_roi
+
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (0, 0, 0), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cv2.imshow("camera", img)
+    k = cv2.waitKey(10) & 0xFF  # Press 'ESC' for exiting video
+    if k == 27:
+        break
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
